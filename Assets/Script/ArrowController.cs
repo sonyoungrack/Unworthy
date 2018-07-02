59ece58e2c95ebbdd4a210ad8f62d821b120678a
Son yeongrak <tdf0495@naver.com>
2018-07-02 22:05:52 +0900

    프리팹등 이것저것 추가

---
+++ Assets/Script/ArrowController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowController : MonoBehaviour {
+    
+    private Rigidbody2D rigid;
+    public float prevY;
+    public float range = 1f;
+	void Start () {
+        rigid = GetComponent<Rigidbody2D>();
+        rigid.AddForce(transform.right*range);
+    }
+    private void Update()
+    {
+    }
+}
---
+++ Assets/Script/MagicEffact.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicEffact : MonoBehaviour {
+    public float smallingStart = 1f;
+    private float delay = 0f;
+    public float smallingSpeedPerSecend = 1;
+	void Update () {
+        delay += Time.deltaTime;
+        if(smallingStart<=delay)
+        {
+            transform.localScale = new Vector3(transform.localScale.x-Time.deltaTime/smallingSpeedPerSecend,
+                transform.localScale.y - Time.deltaTime / smallingSpeedPerSecend,
+                transform.localScale.z - Time.deltaTime / smallingSpeedPerSecend);
+            if(transform.localScale.x<=0)
+            {
+                Destroy(gameObject);
+            }
+        }
+	}
+}
