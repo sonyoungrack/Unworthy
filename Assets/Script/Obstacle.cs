f57f21a5a3ac81a5a65451affa907cd2bb8f52da
Son yeongrak <tdf0495@naver.com>
2018-07-10 01:46:01 +0900

    가시 구현 및 닻 추가 맞을시 즉사 (사망모션 추가 요청)

---
+++ Assets/Script/Obstacle.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ObstacleType
+{
+    Thorn,
+    Anchor
+}
+public class Obstacle : MonoBehaviour {
+
+    public ObstacleType type;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag=="Player")
+        {
+            var go = collision.gameObject.GetComponent<Player>();
+            go.MinusHealth(go.maxHealth);
+        }
+    }
+}
